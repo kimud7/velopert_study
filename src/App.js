cc25ac4d31233f32186c439ac065ba1edb3b49d0
KUD <kimud6003@gmail.com>
2020-10-17 22:40:22 +0900

    useState_basic_Counter

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Chapter1 from './chapter1'
 function App() {
   return (
     <div>
-      <Chapter1 test="hello"  color="blue">asd</Chapter1>
-      <Chapter1 test="hello2"  ></Chapter1>
+      <Chapter1></Chapter1>
     </div>
   );
 }
--- src/chapter1.js
+++ src/chapter1.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
-const chapter = (props) => {
+const Counter = (props) => { //컴포넌트는 항상 대문자를 하는 버릇을 들이자
+   /* const numberState = useState(0);
+    const number = numberState[0];
+    const setNumber = numberState[1]; */
+    const [Number, setNumber] = useState(0);
+
+    const increase = () => {
+        setNumber(Number+1);
+    }
+    const decrease = () => {
+        setNumber(Number-1);
+    }
     return (
-        <h1 style={{color: props.color}}>{props.children}</h1>
+        <div>
+            <div> 카운터 </div>
+            <h1>{Number}</h1>
+            <button onClick={increase(1)}>+</button>
+            <button onClick={decrease(-1)}>-</button>
+        </div>
       );
 }
- 
-chapter.defaultProps = {
-    color: 'red'
-}
-export default chapter;
+
+export default Counter;
