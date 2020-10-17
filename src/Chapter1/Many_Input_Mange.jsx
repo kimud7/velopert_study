dd7d74662ed71db6a4854d85ffc41604dfe6dff3
KUD <kimud6003@gmail.com>
2020-10-17 23:59:12 +0900

    UseRef_use

--- src/Chapter1/Many_Input_Mange.jsx
+++ src/Chapter1/Many_Input_Mange.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 
 const Many_Input = () => {
     const [inputs, setinputs] = useState({
         name:'',
         nickname:''
     });
+
+    const nameInput = useRef();
 
     const {name,nickname} = inputs; //값 꺼내오기
 
@@ -22,12 +24,13 @@ const Many_Input = () => {
         setinputs({
           name: '',
           nickname: '',
-        })
+        });
+        nameInput.current.focus();
       };
     return ( 
     <div>
         {/* name은 넣고자하는 변수이름 value는 변수에 넣고자 하는 값 */}
-      <input name="name" placeholder="이름"  onChange={ReactChange} value={name} /> 
+      <input name="name" placeholder="이름"  onChange={ReactChange} value={name} ref={nameInput} /> 
       <input name="nickname" placeholder="닉네임" onChange={ReactChange} value={nickname}  />
       <button onClick={onReset} >초기화</button>
       <div>
