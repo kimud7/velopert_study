d54c41ff2621b3ceaf6f9f11fe4392c29bce6076
KUD <kimud6003@gmail.com>
2020-10-18 00:18:46 +0900

    Array_Manage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Chapter1_UseState from './Chapter1/Use_State_Counter'
 import Chapter1_Input_Mange from './Chapter1/Input_Mange'
 import Chapter1_Many_Input_Mange from './Chapter1/Many_Input_Mange'
+import Chapter1_Array_Mange from './Chapter1/Array_Mange'
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div>
       {/* <Chapter1_UseState></Chapter1_UseState> */}
       {/* <Chapter1_Input_Mange></Chapter1_Input_Mange> */}
-      <Chapter1_Many_Input_Mange></Chapter1_Many_Input_Mange>
+      {/* <Chapter1_Many_Input_Mange></Chapter1_Many_Input_Mange> */}
+      <Chapter1_Array_Mange></Chapter1_Array_Mange>
     </div>
   );
 }
